c6fe8eac0434c9e5b1d8dea6dfdeb9623a04e688
HERB Geoffroy <geoffroy.herb@hotmail.fr>
2022-06-07 18:19:21 +0200

    update ci for db initialization

---
+++ src/postgres/01_Insert_Annonce.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO antigaspi."Annonces" ("Id", "Address", "Availability", "CompanyName", "Description", "Email", "Expiration", "Status", "Title")
+VALUES ('e4a092f5-817f-4876-a62d-aa0e3a77deb9', 'MyAddress', '2022-06-07 15:14:37.909+00', 'MyCompany', 'MyDescription', 'email@soat.fr', '2022-06-07 15:14:37.909+00', 0, 'MyTitle');
