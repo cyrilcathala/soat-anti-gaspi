a546ef13bff0e0b2f1a518ca15696afcf43120f6
Cyril Cathala <cyril.cathala@outlook.com>
2022-09-24 23:10:43 +0200

    Add state

--- deploy/providers.tf
+++ deploy/providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~>3.0"
     }
   }
+  backend "azurerm" {
+      resource_group_name  = "rg-soat-bc22-dev-fr"
+      storage_account_name = "stsoatbc22devfr"
+      container_name       = "terraform"
+      key                  = "terraform.tfstate"
+    }
 }
 
 provider "azurerm" {
