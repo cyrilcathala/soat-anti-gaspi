171cb7aeea02578ba3a4da6031981c7528948ed5
Cyril Cathala <cyril.cathala@outlook.com>
2022-09-24 21:59:44 +0200

    Test

--- deploy/output.tf
+++ deploy/output.tf
@@ -14,3 +14,21 @@ output "potgresql_connection_string" {
   value = "Server=${azurerm_postgresql_flexible_server.default.name}.postgres.database.azure.com;Database=${azurerm_postgresql_flexible_server_database.default.name};Username=${azurerm_postgresql_flexible_server.default.administrator_login};Password=${azurerm_postgresql_flexible_server.default.administrator_password}"
   sensitive = true
 }
+
+output "potgresql_url" {
+  value = "${azurerm_postgresql_flexible_server.default.name}.postgres.database.azure.com"
+}
+
+output "postgresql_database_name" {
+  value = azurerm_postgresql_flexible_server_database.default.name
+}
+
+output "potgresql_user" {
+  value = azurerm_postgresql_flexible_server.default.administrator_login
+}
+
+output "potgresql_password" {
+  value = azurerm_postgresql_flexible_server.default.administrator_password
+  sensitive = true
+}
+
