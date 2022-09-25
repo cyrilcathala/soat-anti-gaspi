808d956e68fb51645027b078a26e6d281ed86076
Cyril Cathala <cyril.cathala@outlook.com>
2022-09-25 10:35:47 +0200

    Fix

--- deploy/output.tf
+++ deploy/output.tf
@@ -6,12 +6,12 @@ output "postgresql_name" {
   value = azurerm_postgresql_flexible_server.default.name
 }
 
-output "potgresql_connection_string" {
+output "postgresql_connection_string" {
   value = "Server=${azurerm_postgresql_flexible_server.default.name}.postgres.database.azure.com;Database=${azurerm_postgresql_flexible_server_database.default.name};Username=${azurerm_postgresql_flexible_server.default.administrator_login};Password=${azurerm_postgresql_flexible_server.default.administrator_password}"
   sensitive = true
 }
 
-output "potgresql_url" {
+output "postgresql_url" {
   value = "${azurerm_postgresql_flexible_server.default.name}.postgres.database.azure.com"
 }
 
@@ -19,11 +19,11 @@ output "postgresql_database_name" {
   value = azurerm_postgresql_flexible_server_database.default.name
 }
 
-output "potgresql_user" {
+output "postgresql_user" {
   value = azurerm_postgresql_flexible_server.default.administrator_login
 }
 
-output "potgresql_password" {
+output "postgresql_password" {
   value = azurerm_postgresql_flexible_server.default.administrator_password
   sensitive = true
 }
