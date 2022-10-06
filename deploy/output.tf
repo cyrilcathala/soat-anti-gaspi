339a10c10097e23cd90c46ed95d6841d88afa7e3
SKUD201 <95476664+SKUD201@users.noreply.github.com>
2022-10-06 10:05:21 +0200

    Revert "Feature/terraform changes"

--- deploy/output.tf
+++ deploy/output.tf
@@ -1,5 +1,5 @@
 output "resource_group_name" {
-  value = "rg-${var.name_suffix}"
+  value = azurerm_resource_group.default.name
 }
 
 output "web_app_name" {
@@ -11,7 +11,7 @@ output "postgresql_name" {
 }
 
 output "postgresql_connection_string" {
-  value     = "Server=${azurerm_postgresql_flexible_server.default.name}.postgres.database.azure.com;Database=${azurerm_postgresql_flexible_server_database.default.name};Username=${azurerm_postgresql_flexible_server.default.administrator_login};Password=${azurerm_postgresql_flexible_server.default.administrator_password}"
+  value = "Server=${azurerm_postgresql_flexible_server.default.name}.postgres.database.azure.com;Database=${azurerm_postgresql_flexible_server_database.default.name};Username=${azurerm_postgresql_flexible_server.default.administrator_login};Password=${azurerm_postgresql_flexible_server.default.administrator_password}"
   sensitive = true
 }
 
@@ -28,7 +28,7 @@ output "postgresql_user" {
 }
 
 output "postgresql_password" {
-  value     = azurerm_postgresql_flexible_server.default.administrator_password
+  value = azurerm_postgresql_flexible_server.default.administrator_password
   sensitive = true
 }
 
